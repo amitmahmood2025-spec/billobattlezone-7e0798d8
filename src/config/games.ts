c197f926e80f911016ec376c863c6ad910207a96
biplobahmed12397-dot <biplobahmed12397@gmail.com>
2026-02-25 03:51:04 +0600

    Create configuration for game modes

---
+++ src/config/games.ts
@@ -0,0 +1,21 @@
+const gameModes = {
+    FreeFire: {
+        BattleRoyale: {},
+        ClashSquad: {},
+        LoneWolf: {},
+        CS: {
+            "1v1": {},
+            "1v2": {},
+            "1v3": {},
+            "1v4": {}
+        }
+    },
+    PUBG: {
+        Squad: {},
+        Duo: {},
+        Solo: {}
+    },
+    Ludo: {}
+};
+
+export default gameModes;
