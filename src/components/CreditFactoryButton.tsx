f3b872f62a14fe4454ea3b5c153fc8b8a63bbade
amitmahmood2025-spec <amitmahmood2025@gmail.com>
2026-03-02 05:57:03 +0600

    Create CreditFactoryButton.tsx

---
+++ src/components/CreditFactoryButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CreditFactoryButton: React.FC = () => {
+    const handleButtonClick = () => {
+        // Logic to open credit factory bot
+    };
+
+    return (
+        <button onClick={handleButtonClick}>
+            Open Credit Factory Bot
+        </button>
+    );
+};
+
+export default CreditFactoryButton;
